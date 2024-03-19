774ac9ce60c13158ad63521155bfc43967bf7965
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-03-18 19:49:10 -0500

    add clerk

--- app/root.tsx
+++ app/root.tsx
@@ -1,29 +1,33 @@
+import { ClerkApp } from "@clerk/remix"
+import { rootAuthLoader } from "@clerk/remix/ssr.server"
 import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+	Links,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+} from "@remix-run/react"
+
+export const loader = rootAuthLoader
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				{children}
+				<ScrollRestoration />
+				<Scripts />
+			</body>
+		</html>
+	)
 }
 
-export default function App() {
-  return <Outlet />;
-}
+export default ClerkApp(function App() {
+	return <Outlet />
+})
